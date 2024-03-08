96d395c7e37a762d19de36647c817c4322da3ecc
RoyS196 <>
2024-03-08 16:56:39 +0100

    add helper functions to obtain Nonogram from NonogramGrid

--- src/helpers.py
+++ src/helpers.py
@@ -7,5 +7,37 @@ def obtain_grid_from_csv(filename: str, delimiter: str) -> NonogramGrid:
     return NonogramGrid(grid_array=grid_array)
 
 
-def obtain_nonogram_from_grid() -> Nonogram:
-    pass
+def obtain_line_clues_from_line(line_array: np.ndarray) -> LineClues:
+    block_sizes = []
+    block_colors = []
+    square_counter = 0
+    previous_square = 0
+
+    for square in line_array:
+        if square != previous_square:
+            if previous_square != 0:
+                block_sizes.append(square_counter)
+                block_colors.append(previous_square)
+            square_counter = 0
+        square_counter += 1
+        previous_square = square
+
+    if previous_square != 0:
+        block_sizes.append(square_counter)
+        block_colors.append(previous_square)
+
+    return LineClues(block_sizes=tuple(block_sizes), line_length=len(line_array), block_colors=tuple(block_colors))
+
+
+def obtain_nonogram_from_grid(nonogram_grid: NonogramGrid) -> Nonogram:
+    grid_array = nonogram_grid.grid_array
+    row_clues_list = []
+    col_clues_list = []
+
+    for row_array in grid_array:
+        row_clues_list.append(obtain_line_clues_from_line(row_array))
+
+    for col_array in grid_array.T:
+        col_clues_list.append(obtain_line_clues_from_line(col_array))
+
+    return Nonogram(row_clues_tuple=tuple(row_clues_list), col_clues_tuple=tuple(col_clues_list))
